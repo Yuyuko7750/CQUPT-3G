fd7d91841f207ba6faf9eae1cd514c7e3b9abaf5
Yuyuko7750 <540457032@qq.com>
2022-12-14 09:36:25 +0800

    [fix]:register [optimize]:rememberPassword

--- app/src/main/java/com/example/cqupt_3g/Load_Activity.java
+++ app/src/main/java/com/example/cqupt_3g/Load_Activity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Load_Activity extends AppCompatActivity {
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private CheckBox rememberPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +26,38 @@ public class Load_Activity extends AppCompatActivity {
         Button buttonRegister=(Button) findViewById(R.id.register);
         EditText loadAccount=(EditText) findViewById(R.id.loadAccount);
         EditText loadPassword=(EditText) findViewById(R.id.loadPassword);
-
         SharedPreferences register=getSharedPreferences("Load_Activity",MODE_PRIVATE);
+
         String a= loadAccount.getText().toString();
         String b= register.getString("account","1");
         String c= loadPassword.getText().toString();
         String d= register.getString("password","0");
 
+        pref= PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPass=(CheckBox)findViewById(R.id.cb_remember_pass);
+        boolean isRemembered=pref.getBoolean("remember_password",false);
+        if(isRemembered){
+             String account =pref.getString("account","");
+             String password=pref.getString("password","");
+             loadAccount.setText(account);
+             loadPassword.setText(password);
+             rememberPass.setChecked(true);
+        }
+
+
         buttonLoad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(a.equals(b)&&c.equals(d)) {
+                    editor=pref.edit();
+                    if(rememberPass.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",(loadAccount.toString()));
+                        editor.putString("password",loadPassword.toString());
+                    }else {
+                        editor.clear();
+                    }
+                    editor.apply();
                     Toast.makeText(Load_Activity.this, "登陆成功", Toast.LENGTH_LONG).show();
                     Intent intent = new Intent(Load_Activity.this, MainActivity.class);
                     startActivity(intent);
